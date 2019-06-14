0547ca615971937928028e43d1c45994e13c2c8e
mattia.mele <mattia.mele@fraternita.it>
2019-06-14 14:18:58 +0200

    Creato Topanomastica

---
+++ TopanomasticaBitss/TopanomasticaClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopanomasticaBitss
+{
+    public class TopanomasticaClient
+    {
+        public List<string> GetNomiList()
+        {
+            return new List<string>()
+            {
+                "Eva Green",
+                "Sharon Stone",
+                "Naomi Campbell"
+            };
+        }
+    }
+}
