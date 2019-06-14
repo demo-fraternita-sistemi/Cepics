eeac23e97d352718ef2b62a712fcfef3cb925b98
mattia.mele <mattia.mele@fraternita.it>
2019-06-14 10:38:09 +0200

    Aggiunto Avvio

---
+++ Avvio/Avvio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Avvio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForegroundColor = ConsoleColor.Yellow;
+            WriteLine("CEPICS v 4.0.162");
+            WriteLine();
+            WriteLine("-----------------------------------------------------");
+            WriteLine();
+            WriteLine("Caricamento Database Completato");
+            WriteLine();
+            WriteLine("Inserire numero carta di credito per proseguire:");
+            ReadLine();
+            WriteLine();
+            WriteLine("Cancellazione Database in corso...");
+            ReadKey();
+
+        }
+    }
+}
