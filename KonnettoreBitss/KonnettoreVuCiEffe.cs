863d13d490ebbd1c2a5b9692956d00de49959693
Mattia Mele <mattia.mele@fraternita.it>
2019-06-14 11:01:13 +0000

    Merged PR 1: Inserito Konnettore
    Inserito Konnettore
    
    Related work items: #1

---
+++ KonnettoreBitss/KonnettoreVuCiEffe.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Drawing;
+
+namespace KonnettoreBitss
+{
+    public class KonnettoreVuCiEffe
+    {
+        public object ImportObject(object obj)
+        {
+            if (IsValid(obj))
+                return obj;
+            else
+            {
+                ((IDisposable)obj).Dispose();
+                return null;
+            }
+        }
+        public bool IsValid(object obj)
+        {
+            if ((Color)obj == Color.Black)
+                return false;
+            else
+                return true;
+        }
+    }
+}
